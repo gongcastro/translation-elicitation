cc478f6e7cb79589bf49ca7c0b2ccc186c1649a3
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-07-25 17:30:14 +0200

    Close workflow and fit new models

--- Code/analysis.R
+++ Code/analysis.R
@@ -3,11 +3,14 @@
 #### set up #########################################
 
 # load packages
-library(tidyverse)  # for basically everything
+library(dplyr)  # for basically everything
+library(tidyr)
+library(ggplot2)
+library(stringr)
 library(patchwork)  # for arranging plots
 library(readxl)     # for importing Excel spreadsheets
 library(purrr)      # for working with lists
-library(janitor)    # for cleaning vairable names
+library(janitor)    # for cleaning variable names
 library(lubridate)  # for working with dates
 library(data.table) # for importing data
 library(stringdist) # for calculating Levenshtein distance
@@ -15,7 +18,7 @@ library(brms)       # for fitting Bayesian models
 library(mice)       # for imputing data
 library(tidybayes)  # for sampling from the posterior in Bayesian models
 library(modelr)     # for predicting scores from models
-library(here)       # for locating files in the repository with reproducibilidy
+library(here)       # for locating files in the repository with reproducibility
 
 # create/load functions
 source(here("Code", "functions.R")) # helper functions
@@ -117,6 +120,7 @@ dat_merged <- dat_processed %>%
                                       country=="UK" & test_language=="Catalan" ~ consec_longest_engspa,
                                       TRUE ~ consec_longest_spacat),
            pthn = ifelse(l1 %in% "ENG", pthn_eng, pthn_spa),
+           freq = ifelse(l1 %in% "ENG", freq_eng, freq_spa),
            accuracy = stringdist(input_text, ort, method = "dl"),
            correct = ifelse(accuracy==0, 1, 0)) %>% 
     select(-matches("engspa|engcat|spacat|phon")) %>% 
@@ -126,7 +130,7 @@ dat_merged <- dat_processed %>%
 fwrite(dat_merged, here("Data", "01_processed.csv"), sep = ",", dec = ".", row.names = FALSE)
 
 #### participant data ###################################
-dat_participants <- read_xlsx(here("Data", "01_processed_coded.xlsx")) %>% 
+dat_participants <- fread(here("Data", "01_processed_coded.csv"), na.strings = "") %>% 
     mutate(valid_response = response_type %in% c("correct", "typo", "wrong", "false_friend"),
            correct_coded = response_type %in% c("correct", "typo")) %>%
     group_by(participant, group) %>%
@@ -143,88 +147,78 @@ dat_participants <- read_xlsx(here("Data", "01_processed_coded.xlsx")) %>%
 valid_participants <- filter(dat_participants, valid_participant) %>% pull(participant)
 
 ####  prepare accuracy data ###############################
-dat_accuracy <- read_xlsx(here("Data", "01_processed_coded.xlsx")) %>% 
+dat_accuracy <- fread(here("Data", "01_processed_coded.csv"), na.strings = "") %>% 
     mutate(valid_response = response_type %in% c("correct", "typo", "wrong", "false_friend"),
            correct_coded = response_type %in% c("correct", "typo"),
-           test_language = as.factor(test_language)) %>%
+           correct_coded = as.numeric(correct_coded),
+           group = as.factor(group)) %>%
     filter(participant %in% valid_participants, # participant is valid
            valid_response, # response is valid 
-           word %!in% practice_trials) %>% # not a practice trial
+           word %!in% practice_trials) %>% # not a practice trial %>% 
+    group_by(trial_id, word, freq, pthn, match_count, similarity_dl, similarity, group) %>% 
+    summarise(correct = sum(correct_coded, na.rm = TRUE),
+              n = n(),
+              proportion = prod(correct, 1/n, na.rm = TRUE),
+              .groups = "drop") %>% 
+    mutate(freq = log10(freq)+3) %>%  
     mutate_at(vars(freq, pthn, similarity, similarity_dl, match_count), function(x) scale(x)[,1]) %>% # standardise continuous predictors
     mutate_at(vars(group), as.factor)
-
-contrasts(dat_accuracy$group) <- contr.helmert(c("UK-SPA", "UK-CAT", "SPA-CAT"))/2
-contrasts(dat_accuracy$shared_onset) <- contr.sum(c(0, 1))/2
 
 #### impute missing data ##################################
 dat_imputed <- mice(dat_accuracy, m = 5, print = FALSE, method = "pmm") %>% 
     complete() %>% 
-    as_tibble()
+    as_tibble() %>% 
+    arrange(trial_id, group)
 
 #### fit models #########################################
-priors_dist <- c(prior(lognormal(0.0774, 0.50), class = "Intercept"), # main intercept
-                 prior(beta(10, 10), class = "zoi"), # distributional parameter for Beta distribution
-                 prior(beta(10, 10), class = "coi"), # distributional parameter for Beta distribution
-                 prior(normal(1.5, 1), class = "phi")) # distributional parameter for Beta distribution
-priors_random <- c(prior(cauchy(0, 5), class = "sd")) # positive SDs
-priors_fixed <- c(prior(normal(0, 5), class = "b")) # fixed and random slopes
+priors <- c(prior(normal(0, 0.001), class = "Intercept"),
+            prior(normal(0, 5), class = "b"), 
+            prior(cauchy(0, 5), class = "sd"))
 
 # null model (only frequency)
-fit0_null <- brm(formula = bf(proportion ~ inv_lo + (1 | group)),
-                 prior = c(priors_dist),
+fit0_null <- brm(formula = bf(correct | trials(n) ~ pthn + (1 | group/trial_id)),
+                 prior = priors,
+                 family = binomial("logit"),
                  data = dat_imputed,
-                 cores = 2,
+                 chains = 1,
+                 iter = 5000,
+                 cores = 4,
                  file = here("Results", "fit0_null.rds"), 
                  control = list(adapt_delta = 0.95),
                  save_model = here("Code", "fit0_null.stan"),
-                 save_all_pars = TRUE,
-                 family = bernoulli("identity"))
-
+                 save_all_pars = TRUE)
+`
 # null model (only frequency)
-fit1_freq <- update(fit0_null, . ~ . + freq + (1 + freq | group),
-                     file = here("Results", "fit1_freq.rds"),
-                     prior = c(priors_dist, priors_random),
+fit1_count <- update(fit0_null, . ~ . -pthn + pthn*match_count - (1 | group/trial_id) + (1 + match_count | group/trial_id),
+                     file = here("Results", "fit1_count.rds"),
+                     prior = c(priors, prior(lkj(2), class = "cor")),
                      newdata = dat_imputed,
-                     save_model = here("Code", "Models", "fit1_freq.stan"))
+                     cores = 4,
+                     save_model = here("Code", "Models", "fit1_count.stan"))
 
 # random intercepts
-fit2_count <- update(fit0_null, . ~ . + freq*match_count - (1 | group) + (1 + freq*match_count | group),
-                     file = here("Results", "fit2_count.rds"),
-                     prior = c(priors_dist, priors_random),
-                     newdata = dat_imputed,
-                     save_model = here("Code", "Models", "fit2_count.stan"))
+fit2_dl <- update(fit0_null, . ~ . -pthn + pthn*similarity_dl - (1 | group/trial_id) + (1 + similarity_dl | group/trial_id),
+                  file = here("Results", "fit2_dl.rds"),
+                  prior = c(priors, prior(lkj(2), class = "cor")),
+                  newdata = dat_imputed,
+                  cores = 4,
+                  save_model = here("Code", "Models", "fit2_dl.stan"))
 
 # match count
-fit3_dl <- update(fit0_null, . ~ . + freq*similarity_dl - (1 | group) + (1 + freq*similarity_dl | group),
-                     file = here("Results", "fit3_dl.rds"),
-                     prior = c(priors_dist, priors_random),
-                     newdata = dat_imputed,
-                     save_model = here("Code", "Models", "fit3_dl.stan"))
-
-# similarity
-fit4_sim <- update(fit0_null, . ~ . + freq*similarity - (1 | group) + (1 + freq*similarity | group),
-                  file = here("Results", "fit4_sim"),
-                  prior = c(priors_dist, priors_random),
-                  newdata = dat_imputed,
-                  save_model = here("Code", "Models", "fit4_sim.stan"))
-
+fit3_sim <- update(fit0_null, . ~ . -pthn + pthn*similarity - (1 | group/trial_id) + (1 + similarity | group/trial_id),
+                   file = here("Results", "fit3_sim.rds"),
+                   prior = c(priors, prior(lkj(2), class = "cor")),
+                   newdata = dat_imputed,
+                   cores = 4,
+                   save_model = here("Code", "Models", "fit3_sim.stan"))
 
 # compute k-fold validation and compare models
-loo_comp <- loo(fit0_null, fit1_groups, fit2_count, fit3_dl, fit4_sim)
-
-# posterior predictive checks (what do our models predict?)
-design_matrix <- expand_grid(group = c("ENG-CAT", "ENG-SPA", "SPA-CAT"),
-                             match_count = unique(dat_imputed$match_count),
-                             similarity = seq_range(dat_imputed$similarity, n = 10),
-                             similarity_dl = seq_range(dat_imputed$similarity, n = 10)) 
-
-int <- mean_hdi(.value, .width = 0.95, .exclude = c(".chain", ".iteration", ".draw", ".row"))
+loo_comp <- loo(fit0_null, fit1_count, fit2_dl, fit3_sim)
 
 
 #### examine posterior #####################################
-
-posterior <- fit4_sim %>% 
-    gather_draws(b_Intercept, b_freq, b_similarity, `b_freq:similarity`, phi, coi, zoi) %>% 
+posterior <- fit3_sim %>% 
+    gather_draws(b_Intercept, b_pthn, b_similarity, `b_pthn:similarity`) %>% 
     ungroup() %>% 
     mutate_at(vars(.chain, .variable), function(x) factor(x, levels = unique(x), ordered = TRUE))
 
@@ -244,74 +238,91 @@ posterior %>%
 
 posterior %>%
     ggplot(aes(.value, .variable)) +
-    #geom_point() +
-    stat_slab(colour = "black") + 
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    stat_slab(fill = "black") + 
+    stat_interval(.width = c(0.95, 0.89, 0.67, 0.5), position = position_nudge(y = -0.15)) +
+    labs(x = "Estimate", y = "Coefficient", colour = "Credible interval",
+         title = "Posterior distribution of coefficients of fixed effects") +
+    scale_colour_brewer(palette = "Blues") +
+    theme_custom +
+    theme(panel.grid.major.y = element_line(colour = "grey"),
+          legend.position = "top",
+          legend.key = element_rect(fill = "transparent")) +
     ggsave(here("Figures", "coefs.png"))
 
-#### Analyse random effects structure #####################
-groups <- fit4_sim %>%
-    spread_draws(r_group[group, parameter]) %>% 
-    median_qi(r_group, .width = .50) %>% 
-    rename(.estimate = r_group) %>%
-    select(-c(.width, .point, .interval)) %>% 
-    pivot_wider(names_from = parameter, values_from = c(.estimate, .lower, .upper)) %>% 
-    clean_names()
 
-group_cors <- fit4_sim %>% 
-    gather_draws(`cor_group__Intercept__freq`,
-                 `cor_group__Intercept__similarity`,
-                 `cor_group__Intercept__freq:similarity`,
-                 `cor_group__freq__similarity`,
-                 `cor_group__freq__freq:similarity`,
-                 `cor_group__similarity__freq:similarity`) %>% 
-    median_qi(.value, .width = .50) %>% 
-    mutate(.variable = str_remove(.variable, ".*?__")) %>% 
-    select(-c(.width, .point, .interval)) %>% 
-    separate(.variable, c("var1", "var2"), "__")
 
-ggplot(groups, aes(x = estimate_intercept, y = estimate_freq, colour = group)) +
-    geom_errorbar(aes(xmin = lower_intercept, xmax = upper_intercept), size = 0.5, width = 0) +
-    geom_errorbar(aes(ymin = lower_freq, ymax = upper_freq), size = 0.5, width = 0) +
-    geom_abline(slope = filter(group_cors, var1 %in% "Intercept", var2 %in% "freq") %>% pull(.value), size = 0.75) +
-    labs(x = "Intercept", y = "Frequency", colour = "Group") +
-    scale_color_brewer(palette = "Set1") +
-    
-    ggplot(groups, aes(x = estimate_intercept, y = estimate_similarity, colour = group)) +
-    geom_errorbar(aes(xmin = lower_intercept, xmax = upper_intercept), size = 0.5, width = 0) +
-    geom_errorbar(aes(ymin = lower_similarity, ymax = upper_similarity), size = 0.5, width = 0) +
-    geom_abline(slope = filter(group_cors, var1 %in% "Intercept", var2 %in% "similarity") %>% pull(.value), size = 0.75) +
-    labs(x = "Intercept", y = "Similarity", colour = "Group") +
-    scale_color_brewer(palette = "Set1") +
-    
-    ggplot(groups, aes(x = estimate_intercept, y = estimate_freq_similarity, colour = group)) +
-    geom_errorbar(aes(xmin = lower_intercept, xmax = upper_intercept), size = 0.5, width = 0) +
-    geom_errorbar(aes(ymin = lower_freq_similarity, ymax = upper_freq_similarity), size = 0.5, width = 0) +
-    geom_abline(slope = filter(group_cors, var1 %in% "Intercept", var2 %in% "freq:similarity") %>% pull(.value), size = 0.75) +
-    labs(x = "Intercept", y = "Frequency:Similarity", colour = "Group") +
-    scale_color_brewer(palette = "Set1") +
-    
-    guide_area() +
-    
-    plot_layout(guides = "collect") &
-    plot_annotation(tag_levels = "A") &
-    theme_custom &
-    theme(legend.key = element_rect(fill = "transparent")) &
-    ggsave(here("Figures", "groups.png"), width = 5)
+#### posterior predictive checks ##########################
 
- dat_accuracy %>%  
-    pivot_longer(data = ., cols = c(match_count, similarity_dl, similarity), names_to = "measure", values_to = "value") %>% 
-    mutate(measure = case_when(measure %in% "match_count" ~ "Match count",
-                               measure %in%  "similarity_dl" ~ "Ort. similarity",
-                               measure %in%  "similarity" ~ "Phon. similarity")) %>% 
-    ggplot(aes(x = value, y = freq)) +
-    facet_grid(measure~group) +
-    stat_density_2d(aes(fill = after_stat(density)), geom = "raster", contour = FALSE) +
-    geom_rug(alpha = 0.25, size = 0.1, length = unit(0.1, "cm")) +
-    geom_smooth(method = "lm", colour = "red", fill = "grey", size = 0.5) +
-    #geom_point(dat = dat_imputed) +
-    labs(x = "Lexical similarity", y = "Frequency", fill = "Density") +
+# posterior predictive checks (what do our models predict?)
+predictions <- expand_grid(n = 1,
+                           pthn = seq_range(dat_imputed$pthn, n = 4),
+                           similarity = seq_range(dat_imputed$similarity, n = 100)) %>% 
+    add_fitted_draws(fit3_sim, newdata = ., re_formula = NA, scale = "response", n = 1000) %>% 
+    mutate(pthn_cat = factor(pthn, levels = unique(.$pthn), labels = paste0("Q", seq(4, 1)))) %>% 
+    ungroup() 
+
+
+predictions %>%
+    ggplot(aes(similarity, .value)) +
+    stat_lineribbon(.width = c(0.95, 0.89, 0.67, 0.5)) +
+    geom_vline(xintercept = 0, colour= "grey") +
+    geom_hline(yintercept = 0.5, colour= "grey") +
+    stat_summary(data = mutate(dat_imputed, pthn = cut_interval(pthn, n = 4, labels = paste0("Q", seq(4, 1)))),
+                 aes(y = proportion), fun = "mean", geom = "point", size = 0.5) +
+    facet_wrap(~pthn_cat) +
+    
+    labs(x = "Phonological similarity", y = "Probability of correct translation",
+         fill = "Credible interval",
+         title = "Effect of phonological similarity on probability of correct translation",
+         subtitle = "Model predictions are displayed for different quartiles of PTHN") +
+    scale_fill_brewer() +
     theme_custom +
     theme(legend.position = "top") +
-    ggsave(here("Figures", "correlations.png"), height = 6)
+    ggsave(here("Figures", "predictions.png"))
 
+#### analyse random effects structure #####################
+cors_items <- spread_draws(fit3_sim, `cor_group:trial_id__Intercept__similarity`) %>%
+    median_hdi() %>% 
+    clean_names()
 
+items <- ranef(fit3_sim)$`group:trial_id` %>% 
+    as_tibble() %>% 
+    clean_names() %>% 
+    rename_all(str_replace, "q2_5", "lower") %>% 
+    rename_all(str_replace, "q97_5", "upper") %>%
+    bind_cols(dat_imputed)
+
+groups <- spread_draws(fit3_sim, r_group[group, param]) %>% 
+    pivot_wider(names_from = param, values_from = r_group) %>% 
+    clean_names()
+
+# item correlations
+ggplot(items, aes(x = estimate_intercept, y = estimate_similarity, colour = group, fill = group)) +
+    geom_point(size = 2, alpha = 0.5, show.legend = FALSE) +
+    geom_smooth(method = "lm", formula = "y ~ x") +
+    labs(x = "Intercept", y = "Similarity slope", colour = "Group", fill = "Group",
+         title = "Correlation between intercepts and similarity slopes",
+         subtitle = paste0("r = ", round(cors_items$cor_group_trial_id_intercept_similarity, 2),
+                           ", Median 95% HDI = [", round(cors_items$lower, 2), "-", round(cors_items$upper, 2), "]",
+                           ", R² = ", round(cors_items$cor_group_trial_id_intercept_similarity^2, 2))) +
+    scale_colour_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = "Set1") +
+    theme_custom +
+    theme(legend.position = c(0.01, 0.9),
+          legend.direction = "horizontal",
+          legend.justification = 0) +
+    ggsave(here("Figures", "random_items.png"))
+
+# group correlations
+ggplot(groups, aes(x = intercept, y = similarity, colour = group)) +
+    facet_wrap(~group) +
+    stat_density_2d_filled(colour = "transparent", contour_var = "ndensity", n = 100, h = 1) +
+    labs(x = "Intercept", y = "Similarity slope", fill = "Probability density") +
+    scale_fill_viridis_d(option = "plasma") +
+    theme_custom +
+    theme(legend.position = "none",
+          panel.border = element_blank()) +
+    ggsave(here("Figures", "random_groups.png"))
+    
+    
--- Code/fit0_null.stan
+++ Code/fit0_null.stan
@@ -1,81 +1,71 @@
-// generated with brms 2.13.0
+// generated with brms 2.12.0
 functions {
-
-  /* zero-one-inflated beta log-PDF of a single response 
-   * Args: 
-   *   y: response value 
-   *   mu: mean parameter of the beta part
-   *   phi: precision parameter of the beta part
-   *   zoi: zero-one-inflation probability
-   *   coi: conditional one-inflation probability
-   * Returns:  
-   *   a scalar to be added to the log posterior 
-   */ 
-   real zero_one_inflated_beta_lpdf(real y, real mu, real phi,
-                                    real zoi, real coi) {
-     row_vector[2] shape = [mu * phi, (1 - mu) * phi]; 
-     if (y == 0) { 
-       return bernoulli_lpmf(1 | zoi) + bernoulli_lpmf(0 | coi); 
-     } else if (y == 1) {
-       return bernoulli_lpmf(1 | zoi) + bernoulli_lpmf(1 | coi);
-     } else { 
-       return bernoulli_lpmf(0 | zoi) + beta_lpdf(y | shape[1], shape[2]);
-     } 
-   }
 }
 data {
   int<lower=1> N;  // number of observations
-  vector[N] Y;  // response variable
+  int Y[N];  // response variable
+  int trials[N];  // number of trials
+  int<lower=1> K;  // number of population-level effects
+  matrix[N, K] X;  // population-level design matrix
   // data for group-level effects of ID 1
   int<lower=1> N_1;  // number of grouping levels
   int<lower=1> M_1;  // number of coefficients per level
   int<lower=1> J_1[N];  // grouping indicator per observation
   // group-level predictor values
   vector[N] Z_1_1;
+  // data for group-level effects of ID 2
+  int<lower=1> N_2;  // number of grouping levels
+  int<lower=1> M_2;  // number of coefficients per level
+  int<lower=1> J_2[N];  // grouping indicator per observation
+  // group-level predictor values
+  vector[N] Z_2_1;
   int prior_only;  // should the likelihood be ignored?
 }
 transformed data {
+  int Kc = K - 1;
+  matrix[N, Kc] Xc;  // centered version of X without an intercept
+  vector[Kc] means_X;  // column means of X before centering
+  for (i in 2:K) {
+    means_X[i - 1] = mean(X[, i]);
+    Xc[, i - 1] = X[, i] - means_X[i - 1];
+  }
 }
 parameters {
+  vector[Kc] b;  // population-level effects
   real Intercept;  // temporary intercept for centered predictors
-  real<lower=0> phi;  // precision parameter
-  real<lower=0,upper=1> zoi;  // zero-one-inflation probability
-  real<lower=0,upper=1> coi;  // conditional one-inflation probability
   vector<lower=0>[M_1] sd_1;  // group-level standard deviations
   vector[N_1] z_1[M_1];  // standardized group-level effects
+  vector<lower=0>[M_2] sd_2;  // group-level standard deviations
+  vector[N_2] z_2[M_2];  // standardized group-level effects
 }
 transformed parameters {
   vector[N_1] r_1_1;  // actual group-level effects
+  vector[N_2] r_2_1;  // actual group-level effects
   r_1_1 = (sd_1[1] * (z_1[1]));
+  r_2_1 = (sd_2[1] * (z_2[1]));
 }
 model {
   // initialize linear predictor term
-  vector[N] mu = Intercept + rep_vector(0, N);
+  vector[N] mu = Intercept + Xc * b;
   for (n in 1:N) {
     // add more terms to the linear predictor
-    mu[n] += r_1_1[J_1[n]] * Z_1_1[n];
-  }
-  for (n in 1:N) {
-    // apply the inverse link function
-    mu[n] = inv_logit(mu[n]);
+    mu[n] += r_1_1[J_1[n]] * Z_1_1[n] + r_2_1[J_2[n]] * Z_2_1[n];
   }
   // priors including all constants
-  target += lognormal_lpdf(Intercept | 0.0774, 0.5);
-  target += normal_lpdf(phi | 1.5, 1)
-    - 1 * normal_lccdf(0 | 1.5, 1);
-  target += beta_lpdf(zoi | 10, 10);
-  target += beta_lpdf(coi | 10, 10);
-  target += student_t_lpdf(sd_1 | 3, 0, 2.5)
-    - 1 * student_t_lccdf(0 | 3, 0, 2.5);
-  target += std_normal_lpdf(z_1[1]);
+  target += normal_lpdf(b | 0, 5);
+  target += normal_lpdf(Intercept | 0, 0.001);
+  target += cauchy_lpdf(sd_1 | 0, 5)
+    - 1 * cauchy_lccdf(0 | 0, 5);
+  target += normal_lpdf(z_1[1] | 0, 1);
+  target += cauchy_lpdf(sd_2 | 0, 5)
+    - 1 * cauchy_lccdf(0 | 0, 5);
+  target += normal_lpdf(z_2[1] | 0, 1);
   // likelihood including all constants
   if (!prior_only) {
-    for (n in 1:N) {
-      target += zero_one_inflated_beta_lpdf(Y[n] | mu[n], phi, zoi, coi);
-    }
+    target += binomial_logit_lpmf(Y | trials, mu);
   }
 }
 generated quantities {
   // actual population-level intercept
-  real b_Intercept = Intercept;
+  real b_Intercept = Intercept - dot_product(means_X, b);
 }
